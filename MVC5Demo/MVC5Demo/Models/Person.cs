b119e748df0d55af009407235ac9df1a593a2a50
km001 <a126515141@gmail.com>
2020-10-18 15:32:35 +0800

    錯訊

--- MVC5Demo/MVC5Demo/Models/Person.cs
+++ MVC5Demo/MVC5Demo/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,8 @@ namespace MVC5Demo.Models
         [Required(ErrorMessage = "請輸入姓名")]
         public string Name { get; set; }
         [Required(ErrorMessage = "請輸入年紀，至少18歲以上")]
+        [Range(18, 90, ErrorMessage = "請輸入年紀，{1}~{2}之間")]
+        [DisplayName("年紀")]
         public int Age { get; set; }
     }
 }
