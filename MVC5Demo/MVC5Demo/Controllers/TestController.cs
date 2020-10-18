dc10ff92ed9a1aefd39f61a42cca563a17e9a057
km001 <a126515141@gmail.com>
2020-10-18 11:03:44 +0800

    + testController

---
+++ MVC5Demo/MVC5Demo/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = new List<Person>()
+            {
+                new Person() { Id = 1, Name = "Will", Age = 18 },
+                new Person() { Id = 2, Name = "Maa", Age = 19 },
+                new Person() { Id = 3, Name = "Ace", Age = 20 },
+                new Person() { Id = 4, Name = "Ite", Age = 21 }
+            };
+
+            return View(data);
+        }
+    }
+}
