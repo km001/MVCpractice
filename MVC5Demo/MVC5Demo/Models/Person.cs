4549a6e2adf0fb67af788a34e27306739166fa71
km001 <a126515141@gmail.com>
2020-10-18 15:06:10 +0800

    Create驗證

--- MVC5Demo/MVC5Demo/Models/Person.cs
+++ MVC5Demo/MVC5Demo/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "請輸入姓名")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "請輸入年紀，至少18歲以上")]
         public int Age { get; set; }
     }
 }
