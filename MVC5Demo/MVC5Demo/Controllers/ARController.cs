0cebc57b07181e049f2d6ba51d32f9dc6ec4dd5b
km001 <a126515141@gmail.com>
2020-10-25 17:19:05 +0800

    各種ActionResult測試

---
+++ MVC5Demo/MVC5Demo/Controllers/ARController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class ARController : BaseController
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewTest2()
+        {
+            return View("Index");
+        }
+
+        public ActionResult ViewTest3()
+        {
+            return View("TEMP");
+        }
+
+        public ActionResult ViewTest4()
+        {
+            return PartialView("Index");//PartialView沒有_layout
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("<root>123</root>", "text/xml", Encoding.GetEncoding("big5"));
+        }
+
+        public ActionResult FileTest(bool dl = false)//可接GET或POST
+        {
+            if (dl)
+            {
+                return File(Server.MapPath("~/Content/bc3b2d8271f43924543571fed081b54e.png"), "image/png", "MyAA.png");//下載，與新黨名
+            }
+            else
+            {
+                return File(Server.MapPath("~/Content/bc3b2d8271f43924543571fed081b54e.png"), "image/png");//只是打開
+            }
+        }
+    }
+}
--- MVC5Demo/MVC5Demo/Global.asax.cs
+++ MVC5Demo/MVC5Demo/Global.asax.cs
@@ -18,6 +18,10 @@ namespace MVC5Demo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //減少搜尋 View 檔案的時間 不找一堆不可能找到的View
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
