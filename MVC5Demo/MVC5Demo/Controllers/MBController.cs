21c9f93e694d1732506b12497d45146028a74f0c
km001 <a126515141@gmail.com>
2020-10-31 12:44:46 +0800

    course 批次更新

--- MVC5Demo/MVC5Demo/Controllers/MBController.cs
+++ MVC5Demo/MVC5Demo/Controllers/MBController.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Omu.ValueInjecter;
 
 namespace MVC5Demo.Controllers
 {
     public class MBController : Controller
     {
+        CourseRepository repoCourse;
         DepartmentRepository repo;
         public MBController()
         {
+            repoCourse = RepositoryHelper.GetCourseRepository();
             repo = RepositoryHelper.GetDepartmentRepository();
         }
         // GET: MB
@@ -39,6 +42,37 @@ namespace MVC5Demo.Controllers
         public ActionResult ReadTempData()
         {
             return View();
+        }
+
+        public ActionResult CourseBatchEdit(bool IsEditMode = false)
+        {
+            ViewData.Model = repoCourse.All();
+
+            ViewBag.IsEditMode = IsEditMode;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CourseBatchEdit(List<CourseBatchEditVM> data, bool IsEditMode = false)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in data)
+                {
+                    var course = repoCourse.All().FirstOrDefault(p => p.CourseID == item.CourseID);
+                    course.InjectFrom(item);
+                }
+                repoCourse.UnitOfWork.Commit();
+
+                TempData["CourseBatchEditResult"] = "批次更新成功！";
+
+                return RedirectToAction("CourseBatchEdit");
+            }
+
+            ViewBag.IsEditMode = IsEditMode;
+
+            return View(repoCourse.All());
         }
     }
 }
--- MVC5Demo/MVC5Demo/Models/Course.Partial.cs
+++ MVC5Demo/MVC5Demo/Models/Course.Partial.cs
@@ -15,6 +15,7 @@ namespace MVC5Demo.Models
         public int CourseID { get; set; }
 
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
         public string Title { get; set; }
         [Required]
         public int Credits { get; set; }
@@ -27,5 +28,18 @@ namespace MVC5Demo.Models
         public virtual Department Department { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
         public virtual ICollection<Person> Teachers { get; set; }
+    }
+
+    public class CourseBatchEditVM//放在一起方便管理
+    {
+        [Required]
+        public int CourseID { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public int Credits { get; set; }
     }
 }
